8187c80b25da3044866b1d015e9a4dbe2a1114d4
dylanson25 <dylanson25@users.noreply.github.com>
2024-10-14 18:59:25 -0700

    [seccion 1] 11 - async await

--- 01-bases/src/bases/10-axion.ts
+++ 01-bases/src/bases/10-axion.ts
@@ -4,14 +4,12 @@ import type { GIFResponse } from "../interfaces/gif.response";
 const apiKey = "";
 const apiUrl = "https://api.giphy.com/v1/gifs/";
 
-const giphyApi = axios.create({
+export const giphyApi = axios.create({
   baseURL: apiUrl,
   params: {
     api_key: apiKey,
   },
 });
-
-// export default giphyApi;
 
 // giphyApi.get<GIFResponse>('/random')
 //   .then( resp => console.log(resp.data.data.images.downsized_large.url))
