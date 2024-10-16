0d925bf9c5356c02bac60a3367dace5e79542f6a
dylanson25 <dylanson25@users.noreply.github.com>
2024-10-15 18:58:14 -0700

    [seccion 2] - Varialbes reactivas

--- 02-intro-vue/app.js
+++ 02-intro-vue/app.js
@@ -2,9 +2,23 @@ const { createApp, ref } = Vue;
 
 const app = createApp({
   template: `
-    <h1>Hola mundo</h1>
-    <p>Desde app.js</p>
+    <h1>{{message}}</h1>
+    <p>{{author}}</p>
     `,
+  setup() {
+    const message = ref("I'm Batman");
+    const author = ref("I'm Batman");
+
+    setTimeout(() => {
+      message.value = "Soy Goku";
+      author.value = "Bruce Walyne";
+    }, 1000);
+
+    return {
+      author,
+      message,
+    };
+  },
 });
 
 app.mount("#app");
