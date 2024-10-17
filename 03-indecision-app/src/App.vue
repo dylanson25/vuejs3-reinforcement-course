656407929a2474aaea6830073f82aaac38e34a1f
dylanson25 <dylanson25@users.noreply.github.com>
2024-10-16 19:22:47 -0700

    [seccion 3] - componentes

--- 03-indecision-app/src/App.vue
+++ 03-indecision-app/src/App.vue
@@ -1,27 +1,11 @@
 <template>
   <main class="container p-4">
-    <section class="card">
-      <div class="card-body">
-        <p><b>Counter:</b> {{ counter }}</p>
-        <p><b>Squere:</b> {{ squereCounter }}</p>
-      </div>
-      <div class="card-footer">
-        <button @click="counter++" class="btn btn-primary me-4">+1</button>
-        <button @click="counter--" class="btn btn-success">-1</button>
-      </div>
-    </section>
+    <MyCounter />
   </main>
 </template>
 
 <script setup lang="ts">
-import { computed, ref } from 'vue';
-
-const counter = ref(0);
-const squereCounter = computed(() => counter.value * counter.value);
+import MyCounter from '@/components/MyCounter.vue';
 </script>
 
-<style scoped>
-.card-body {
-  max-width: 300px;
-}
-</style>
+<style scoped></style>
---
+++ 03-indecision-app/src/components/MyCounter.vue
@@ -0,0 +1,26 @@
+<template>
+  <section class="card">
+    <div class="card-body">
+      <p><b>Counter:</b> {{ counter }}</p>
+      <hr />
+      <p><b>Squere:</b> {{ squereCounter }}</p>
+    </div>
+    <div class="card-footer">
+      <button @click="counter++" class="btn btn-primary me-4">+1</button>
+      <button @click="counter--" class="btn btn-success">-1</button>
+    </div>
+  </section>
+</template>
+
+<script setup lang="ts">
+import { computed, ref } from 'vue';
+
+const counter = ref(0);
+const squereCounter = computed(() => counter.value * counter.value);
+</script>
+
+<style scoped>
+.card-body {
+  max-width: 300px;
+}
+</style>
