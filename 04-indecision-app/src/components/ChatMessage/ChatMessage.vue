8e425449932a7c9bb9906bf229b690d96e11092a
dylanson25 <dylanson25@users.noreply.github.com>
2024-10-19 19:27:26 -0700

    [seccion 4] - referencias a elementos  HTML

--- 04-indecision-app/src/components/ChatMessage/ChatMessage.vue
+++ 04-indecision-app/src/components/ChatMessage/ChatMessage.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex-1 overflow-y-auto p-4">
+  <div ref="chatRef" class="flex-1 overflow-y-auto p-4">
     <div class="flex flex-col space-y-2">
       <!-- Messages go here -->
       <!-- v-for="{ id, message, image, itsMine } in messages" -->
@@ -18,8 +18,20 @@
 <script lang="ts" setup>
 import ChatBubble from '../ChatBubble.vue/ChatBubble.vue'
 import { type ChatMessage } from '../../interfaces/chat-message.interface'
+import { ref, watch } from 'vue'
 interface Props {
   messages: ChatMessage[]
 }
-defineProps<Props>()
+const props = defineProps<Props>()
+
+const chatRef = ref<HTMLDivElement | null>(null)
+
+watch(props.messages, () => {
+  setTimeout(() => {
+    chatRef.value?.scrollTo({
+      top: chatRef.value.scrollHeight,
+      behavior: 'smooth',
+    })
+  }, 100)
+})
 </script>
